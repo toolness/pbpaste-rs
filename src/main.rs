e44c5464c1db49d98c185b67f5bf9fa1a39a4a9e
Atul Varma <varmaa@gmail.com>
2015-03-21 08:29:49 -0400

    factor out a get_clipboard_text() function.

--- src/main.rs
+++ src/main.rs
@@ -32,6 +32,41 @@ fn help(exit_code: i32) {
     std::env::set_exit_status(exit_code);
 }
 
+fn get_clipboard_text(strip_cr: bool) -> Option<Vec<u8>> {
+    let slice_bytes: &[u8];
+
+    unsafe {
+        let success = OpenClipboard(0 as winapi::HWND);
+        if success == 0 {
+            panic!("OpenClipboard() failed!");
+        }
+        let data = GetClipboardData(windows_clipboard_types::CF_TEXT);
+        if data.is_null() {
+            CloseClipboard();
+            return None;
+        }
+        slice_bytes = CStr::from_ptr(data as *const i8).to_bytes();
+    }
+
+    let mut clipboard: Vec<u8> = Vec::with_capacity(slice_bytes.len());
+
+    for i in slice_bytes {
+        if strip_cr {
+            if *i != 13 {
+                clipboard.push(*i);
+            }
+        } else {
+            clipboard.push(*i);
+        }
+    }
+
+    unsafe {
+        CloseClipboard();
+    }
+
+    Some(clipboard)
+}
+
 fn main() {
     let strip_cr: bool;
     let args: Vec<String> = env::args().collect();
@@ -54,49 +89,23 @@ fn main() {
         _ => { return help(1); }
     }
 
-    let slice_bytes: &[u8];
-
-    unsafe {
-        let success = OpenClipboard(0 as winapi::HWND);
-        if success == 0 {
-            panic!("OpenClipboard() failed!");
-        }
-        let data = GetClipboardData(windows_clipboard_types::CF_TEXT);
-        if data.is_null() {
-            CloseClipboard();
-            return;
-        }
-        slice_bytes = CStr::from_ptr(data as *const i8).to_bytes();
-    }
-
-    let mut clipboard: Vec<u8> = Vec::with_capacity(slice_bytes.len());
-
-    for i in slice_bytes {
-        if strip_cr {
-            if *i != 13 {
-                clipboard.push(*i);
-            }
-        } else {
-            clipboard.push(*i);
-        }
-    }
-
-    unsafe {
-        CloseClipboard();
-    }
-
-    match stdout().write_all(clipboard.as_slice()) {
-        Ok(_) => {
-            match stdout().flush() {
+    match get_clipboard_text(strip_cr) {
+        Some(clipboard_text) => {
+            match stdout().write_all(clipboard_text.as_slice()) {
                 Ok(_) => {
+                    match stdout().flush() {
+                        Ok(_) => {
+                        },
+                        Err(_) => {
+                            panic!("flush() failed!");
+                        }
+                    }
                 },
                 Err(_) => {
-                    panic!("flush() failed!");
+                    panic!("write_all() failed!");
                 }
             }
         },
-        Err(_) => {
-            panic!("write_all() failed!");
-        }
+        None => {}
     }
 }
