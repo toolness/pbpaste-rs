546c8c2630e335e34c2d6f2f731d461f26ef27df
Atul Varma <varmaa@gmail.com>
2015-03-21 20:54:26 -0400

    added test

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 #![feature(io)]
 #![feature(core)]
 #![feature(exit_status)]
+#![feature(libc)]
+#![allow(dead_code)]
 
 extern crate "user32-sys" as user32;
 extern crate winapi;
@@ -106,5 +108,78 @@ fn main() {
             }
         },
         None => {}
+    }
+}
+
+mod windows_gmem_types {
+    pub static GMEM_MOVEABLE: u32 = 0x0002;
+}
+
+#[link(name = "kernel32")]
+extern "system" {
+    fn GlobalAlloc(uFlags: winapi::UINT,
+                   dwBytes: winapi::SIZE_T) -> winapi::HGLOBAL;
+
+    fn GlobalLock(hMem: winapi::HGLOBAL) -> winapi::LPVOID;
+    fn GlobalUnlock(hMem: winapi::HGLOBAL) -> winapi::BOOL;
+}
+
+#[link(name = "user32")]
+extern "system" {
+    fn SetClipboardData(uFormat: winapi::UINT,
+                        hMem: winapi::HANDLE) -> winapi::HANDLE;
+}
+
+
+extern crate libc;
+use libc::strcpy;
+use std::ffi::CString;
+
+#[test]
+fn it_works() {
+    fn set_clipboard_text(test_str: &str) {
+        let test_cstring = CString::new(test_str).unwrap();
+
+        unsafe {
+            if OpenClipboard(0 as winapi::HWND) == 0 {
+                panic!("OpenClipboard() failed!");
+            }
+            user32::EmptyClipboard();
+
+            let copy = GlobalAlloc(windows_gmem_types::GMEM_MOVEABLE,
+                                   (test_str.len() + 1) as winapi::SIZE_T);
+            if copy.is_null() {
+                panic!("GlobalAlloc() failed!");
+            }
+            let str_copy = GlobalLock(copy);
+            if str_copy.is_null() {
+                panic!("GlobalLock() failed!");
+            }
+            strcpy(str_copy as *mut i8, test_cstring.as_ptr());
+            GlobalUnlock(copy);
+            if SetClipboardData(windows_clipboard_types::CF_TEXT,
+                                copy).is_null() {
+                panic!("SetClipboardData() failed!");
+            }
+            CloseClipboard();
+        }
+    }
+
+    set_clipboard_text("hello there");
+
+    match get_clipboard_text(false) {
+        Some(vec) => {
+            let utf8 = std::str::from_utf8(vec.as_slice()).unwrap();
+            match utf8 {
+                "hello there" => {
+                },
+                _ => {
+                    panic!("expected clipboard to contain expected text");
+                }
+            }
+        },
+        None => {
+            panic!("expected clipboard to contain text");
+        }
     }
 }
